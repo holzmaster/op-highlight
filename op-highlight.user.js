a171eb6bd52e425bc52454a8e8da26a287a77bc4
holzmaster <holzmaster@outlook.com>
2015-01-18 08:22:58 +0100

    Revert "Using GM_addStyle"
    This reverts commit 53a21ff9a2bc2f245e06d9a595ed051eebb4eba8.

--- op-highlight.user.js
+++ op-highlight.user.js
@@ -3,13 +3,13 @@
 // @author		holzmaster
 // @namespace	holzmaster
 // @include		http://pr0gramm.com*
-// @version		1.0.2
+// @version		1.0.1
 // @updateURL	https://raw.githubusercontent.com/pr0nopoly/op-highlight/master/op-highlight.user.js
 // @downloadURL	https://raw.githubusercontent.com/pr0nopoly/op-highlight/master/op-highlight.user.js
 // @copyright	2014+, holzmaster
 // @description	Hebt den OP eines Bildes in den Kommentaren hervor.
 // @icon		http://pr0gramm.com/media/pr0gramm-favicon.png
-// @grant		GM_addStyle
+// @grant		none
 // ==/UserScript==
 
 (function() {
@@ -22,13 +22,24 @@
 		return currentOp == currentUser ? " " + opClassName : "";
 	};
 
+	function addGlobalStyle(css) {
+		var head = document.getElementsByTagName("head")[0];
+		if (!head)
+			return;
+
+		var style = document.createElement("style");
+		style.type = "text/css";
+		style.innerHTML = css;
+		head.appendChild(style);
+	}
+
 	$(function() {
 		p.View.Stream.Comments.prototype.template =
 		p.View.Stream.Comments.prototype.template
 			.replace('class="comment{p.voteClass(c.vote)}',
 					 'class="comment{p.voteClass(c.vote)}{p.opClass(itemOp,c.name)}');
 
-		GM_addStyle(cssRule);
+		addGlobalStyle(cssRule);
 
 		var oldLoaded = p.View.Stream.Comments.prototype.loaded;
 		p.View.Stream.Comments.prototype.loaded = function(item) {
