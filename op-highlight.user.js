53a21ff9a2bc2f245e06d9a595ed051eebb4eba8
pr0nopoly <pr0nopoly@outlook.com>
2015-01-18 08:18:59 +0100

    Using GM_addStyle

--- op-highlight.user.js
+++ op-highlight.user.js
@@ -3,13 +3,13 @@
 // @author		holzmaster
 // @namespace	holzmaster
 // @include		http://pr0gramm.com*
-// @version		1.0.1
+// @version		1.0.2
 // @updateURL	https://raw.githubusercontent.com/pr0nopoly/op-highlight/master/op-highlight.user.js
 // @downloadURL	https://raw.githubusercontent.com/pr0nopoly/op-highlight/master/op-highlight.user.js
 // @copyright	2014+, holzmaster
 // @description	Hebt den OP eines Bildes in den Kommentaren hervor.
 // @icon		http://pr0gramm.com/media/pr0gramm-favicon.png
-// @grant		none
+// @grant		GM_addStyle
 // ==/UserScript==
 
 (function() {
@@ -22,24 +22,13 @@
 		return currentOp == currentUser ? " " + opClassName : "";
 	};
 
-	function addGlobalStyle(css) {
-		var head = document.getElementsByTagName("head")[0];
-		if (!head)
-			return;
-
-		var style = document.createElement("style");
-		style.type = "text/css";
-		style.innerHTML = css;
-		head.appendChild(style);
-	}
-
 	$(function() {
 		p.View.Stream.Comments.prototype.template =
 		p.View.Stream.Comments.prototype.template
 			.replace('class="comment{p.voteClass(c.vote)}',
 					 'class="comment{p.voteClass(c.vote)}{p.opClass(itemOp,c.name)}');
 
-		addGlobalStyle(cssRule);
+		GM_addStyle(cssRule);
 
 		var oldLoaded = p.View.Stream.Comments.prototype.loaded;
 		p.View.Stream.Comments.prototype.loaded = function(item) {
