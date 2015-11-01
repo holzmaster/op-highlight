d9b4bb39d1f201826e51ea484e0611a8504a8f62
holzmaster <holzmaster@outlook.com>
2015-11-01 02:09:07 +0100

    Update update url

--- op-highlight.user.js
+++ op-highlight.user.js
@@ -5,8 +5,8 @@
 // @include		http://pr0gramm.com*
 // @include		https://pr0gramm.com*
 // @version		1.1.2
-// @updateURL	https://raw.githubusercontent.com/pr0nopoly/op-highlight/master/op-highlight.user.js
-// @downloadURL	https://raw.githubusercontent.com/pr0nopoly/op-highlight/master/op-highlight.user.js
+// @updateURL	https://raw.githubusercontent.com/holzmaster/op-highlight/master/op-highlight.user.js
+// @downloadURL	https://raw.githubusercontent.com/holzmaster/op-highlight/master/op-highlight.user.js
 // @copyright	2014+, holzmaster
 // @description	Hebt den OP eines Bildes in den Kommentaren hervor.
 // @icon		http://pr0gramm.com/media/pr0gramm-favicon.png
