b8659bc0855e459905b39378bd53efad4c495093
holzmaster <holzmaster@outlook.com>
2016-06-27 16:38:11 +0200

    Fix exception

--- op-highlight.user.js
+++ op-highlight.user.js
@@ -4,7 +4,7 @@
 // @namespace	holzmaster
 // @include		http://pr0gramm.com*
 // @include		https://pr0gramm.com*
-// @version		1.1.5
+// @version		1.2.0
 // @updateURL	https://holzmaster.github.io/op-highlight/op-highlight.user.js
 // @downloadURL	https://holzmaster.github.io/op-highlight/op-highlight.user.js
 // @copyright	2014+, holzmaster
@@ -13,32 +13,23 @@
 // @grant		none
 // ==/UserScript==
 
-(function() {
+function addGlobalStyle(css) {
+	const style = document.createElement('style');
+	style.type = 'text/css';
+	style.innerHTML = css;
+	document.head.appendChild(style);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
 	"use strict";
 
-	var opClassName = "extension-is-op"
-	var cssRule = "." + opClassName + " .user:before { content: 'OP'; color: #FFF; padding: 1px 6px; vertical-align: baseline; text-align: center; font-weight: bold; border-radius: 0.25em; background-color: rgb(238, 77, 46); margin-right: 5px; }";
+	const cssRule = ".extension-is-op .user:before { content: 'OP'; color: #FFF; padding: 1px 6px; vertical-align: baseline; text-align: center; font-weight: bold; border-radius: 0.25em; background-color: rgb(238, 77, 46); margin-right: 5px; }";
+	p.View.Stream.Comments.prototype.template =
+	p.View.Stream.Comments.prototype.template
+		.replace('<?js if(c.name == itemUser){?> <span class="user-comment-op">OP</span><?js}?>', '') /* remove features that uses span element */
+		.replace('class="comment{p.voteClass(c.vote)}',
+				 'class="comment{p.voteClass(c.vote)}<?js if(c.name == itemUser){?> extension-is-op<?js}?>'); /* use css-content instead */
 
-	function addGlobalStyle(css) {
-		const style = document.createElement('style');
-		style.type = 'text/css';
-		style.innerHTML = css;
-		document.head.appendChild(style);
-	}
+	addGlobalStyle(cssRule);
+});
 
-	$(function() {
-		p.View.Stream.Comments.prototype.template =
-		p.View.Stream.Comments.prototype.template
-			.replace('<?js if(c.name == op){?> <span class="user-comment-op">OP</span><?js}?>', '') /* remove features that uses span element */
-			.replace('class="comment{p.voteClass(c.vote)}',
-					 'class="comment{p.voteClass(c.vote)}<?js if(c.name == op){?> ' + opClassName + '<?js}?>'); /* use css-content instead */
-
-		addGlobalStyle(cssRule);
-
-		var oldLoaded = p.View.Stream.Comments.prototype.loaded;
-		p.View.Stream.Comments.prototype.loaded = function(item) {
-			this.data.itemOp = this.data.itemOp || item.user || null;
-			oldLoaded.apply(this, arguments);
-		};
-	});
-})();
